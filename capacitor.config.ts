7c90a33457acc0fe855ec5d39f14af03272ecc1e
Nestor Kenneth Aniar <ken.aniar.dev@gmail.com>
2025-07-12 21:21:12 +0800

    added capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.selfbetapp',
+  appName: 'self bet app',
+  webDir: 'dist'
+};
+
+export default config;
