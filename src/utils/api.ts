8f30257285eddfce5a2afc7d24dc86da274663bc
Nestor Kenneth Tesorio Aniar <68641803+KenAniarDev@users.noreply.github.com>
2025-08-22 00:24:30 +0800

    Update api.ts

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,6 +1,6 @@
 import { auth } from "@/firebase/config";
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://localhost:7192",
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://localhost:7192";
 
 interface ApiResponse<T = unknown> {
   data?: T;
